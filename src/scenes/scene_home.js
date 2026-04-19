50691eca5e464bab174698dbf2d02f190b2e4270
Yasser Muayed <yassermuayed@gmail.com>
2026-04-19 22:12:34 +0300

    update branding

--- src/scenes/scene_home.js
+++ src/scenes/scene_home.js
@@ -2,7 +2,6 @@ import { SceneManagerInstance, StateManagerInstance } from "../main.js";
 import { button, comment, cont } from "../components/blocks.js";
 import _Scene from "../engine/_scene";
 import GameScene from "./scene_game";
-import SettingsScene from "./scene_settings";
 import smallLogoUTL from "../../public/text-logo.webp";
 export default class HomeScene extends _Scene {
   constructor() {
@@ -29,30 +28,6 @@ export default class HomeScene extends _Scene {
         this.add(comment(StateManagerInstance.personalBest)),
       ]),
       ["high-score"]
-    );
-    let shareData = {
-      title: "Simon Memory Challenge",
-      text: "Get Simon Memory Challenge on Google Play Now!",
-      url: "https://play.google.com/store/apps/details?id=com.yassermuayed.ssr",
-    };
-    let shareBtn = document.createElement("button");
-    shareBtn.textContent = "Share";
-    shareBtn.classList.add("btn");
-    shareBtn.addEventListener("click", async () => {
-      try {
-        await navigator.share(shareData);
-      } catch (err) {
-        console.log(err);
-      }
-    });
-    this.add(
-      cont([
-        shareBtn,
-        button("About", () =>
-          SceneManagerInstance.changeScene(new SettingsScene())
-        ),
-      ]),
-      ["flex", "bottom"]
     );
   }
 }
